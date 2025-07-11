7cf596617f7560e67d258c9e0a9420c9f5d0b592
halimShabalout <halim.shabalout.computer@gmail.com>
2025-07-11 15:50:01 +0300

    feat: implement category CRUD operations

--- src/common/constants/messages.enum.ts
+++ src/common/constants/messages.enum.ts
@@ -6,4 +6,12 @@ export enum Messages {
   USER_UPDATED = 'User updated successfully.',
   USER_DELETED = 'User deleted successfully.',
   USER_NOT_FOUND = 'User not found.',
+
+  // Category Messages
+  CATEGORY_CREATED = 'Category created successfully.',
+  CATEGORY_RETRIEVED = 'Category retrieved successfully.',
+  CATEGORIES_RETRIEVED = 'Categories retrieved successfully.',
+  CATEGORY_UPDATED = 'Category updated successfully.',
+  CATEGORY_DELETED = 'Category deleted successfully.',
+  CATEGORY_NOT_FOUND = 'Category not found.',
 }
